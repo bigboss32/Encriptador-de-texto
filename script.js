1db34e8866d167824d5586a3effcc88ec6c3145f
Miguel Angel Garzon Sanabria <mig.garzon@udla.edu.co>
2023-05-09 15:05:34 -0500

    Signed-off-by: Miguel Angel Garzon Sanabria <mig.garzon@udla.edu.co>

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 function encriptar() {
+  var pattern = "^*@!#$%&/()=?¡!¿";
   var texto = document.getElementById("inputtexto");
+  var bandera = true
 
-  var a = "";
-  for (var i = 0; i < texto.value.length; i++) {
-    var caracter = texto.value.charAt(i);
-    console.log(caracter);
-    if (caracter == "a") {
-      caracter = "ai";
-    }
-    if (caracter == "e") {
-      caracter = "enter";
-    }
-    if (caracter == "i") {
-      caracter = "imes";
-    }
-    if (caracter == "o") {
-      caracter = "ober";
-    }
-    if (caracter == "u") {
-      caracter = "ufat";
-    }
-    a = a + "" + caracter;
+  if (texto.value.includes(pattern)) {
+    console.log("esta")
   }
-  alert(a.toString());
-  document.getElementById("imagen-mu").style.display = "none";
-  document.getElementById("text").style.display = "none";
-  document.getElementById("copiar").style.display = "show";
-  document.getElementById("textodos").innerHTML = a;
-  document.getElementById("inputtexto").innerHTML = " ";
-  document.getElementById("copiar").style.display = "inherit";
+
+  for (var i = 0; i < pattern.length; i++) {
+
+
+    if (texto.value.includes(pattern.charAt(i))) {
+      alert("Carcater especial");
+      bandera = false
+    }
+
+  }
+
+  if (bandera) {
+    var a = "";
+    for (var i = 0; i < texto.value.length; i++) {
+      var caracter = texto.value.charAt(i);
+      console.log(caracter);
+      if (caracter == "a") {
+        caracter = "ai";
+      }
+      if (caracter == "e") {
+        caracter = "enter";
+      }
+      if (caracter == "i") {
+        caracter = "imes";
+      }
+      if (caracter == "o") {
+        caracter = "ober";
+      }
+      if (caracter == "u") {
+        caracter = "ufat";
+      }
+      a = a + "" + caracter;
+    }
+
+    document.getElementById("imagen-mu").style.display = "none";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("textodos").innerHTML = a;
+    document.getElementById("inputtexto").innerHTML = " ";
+    document.getElementById("copiar").style.display = "inherit";
+  }
+
 }
 
 function desencriptar() {
@@ -49,5 +68,5 @@ function copiar() {
   var contenido = document.querySelector("#textodos");
   contenido.select();
   document.execCommand("copy");
-  alert("Copied!");
+  alert("copiado!");
 }
